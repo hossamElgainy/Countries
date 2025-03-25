a624b9c080d5a09f987d733c61a011b6c18b19b2
hossam elsayed <hossamelsayed676@gmail.com>
2025-03-25 22:51:36 +0200

    Add All Files

---
+++ Countries/Countries/Controllers/CountriesController.cs
@@ -0,0 +1,69 @@
+﻿using CountryData.Standard;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Countries.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult GetCountries()
+        {
+            var helper = new CountryHelper();
+            return Ok(helper.GetCountries());
+        }
+        [HttpGet("[action]")]
+        public IActionResult GetCountryData()
+        {
+            var helper = new CountryHelper();
+            return Ok(helper.GetCountryData());
+        }
+        [HttpGet("[action]")]
+        public IActionResult GetCountryByCode(string code)
+        {
+            var helper = new CountryHelper();
+            return Ok(helper.GetCountryByCode(code));
+        }
+        [HttpGet("[action]")]
+        public IActionResult GetCountryByCurrencyCode(string code)
+        {
+            var helper = new CountryHelper();
+            return Ok(helper.GetCountryByCurrencyCode(code));
+        }
+        [HttpGet("[action]")]
+        public IActionResult GetCountryByPhoneCode(string code)
+        {
+            var helper = new CountryHelper();
+            return Ok(helper.GetCountryByPhoneCode(code));
+        }
+        [HttpGet("[action]")]
+        public IActionResult GetCountryEmojiFlag(string code)
+        {
+            var helper = new CountryHelper();
+
+            return Ok(helper.GetCountryEmojiFlag(code));
+
+        }[HttpGet("[action]")]
+        public IActionResult GetCurrencyCodesByCountryCode(string code)
+        {
+            var helper = new CountryHelper();
+            return Ok(helper.GetCurrencyCodesByCountryCode(code));
+        }
+        [HttpGet("[action]")]
+        public IActionResult GetPhoneCodeByCountryShortCode(string code)
+        {
+            var helper = new CountryHelper();
+
+            return Ok(helper.GetPhoneCodeByCountryShortCode(code));
+        }
+        [HttpGet("[action]")]
+        public IActionResult GetRegionByCountryCode(string code)
+        {
+            var helper = new CountryHelper();
+
+            return Ok(helper.GetRegionByCountryCode(code));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# Countries
+# Countries API
+
+This project provides a set of APIs to retrieve information about countries, including country data, currency codes, phone codes, and more.
+
+## Technologies Used
+
+- C# 12.0
+- .NET 8
+- ASP.NET Core
+
+## Getting Started
+
+### Prerequisites
+
+- .NET 8 SDK
+
+### Installation
+
+1. Clone the repository:
+2. Navigate to the project directory:
+3. Restore the dependencies:
+### Running the Application
+
+To run the application locally, use the following command:
+The API will be available at `https://localhost:5001/api/countries`.
+
+## API Endpoints
+
+- `GET /api/countries/GetCountries` - Retrieves a list of all countries.
+- `GET /api/countries/GetCountryData` - Retrieves detailed data for all countries.
+- `GET /api/countries/GetCountryByCode?code={code}` - Retrieves country information by country code.
+- `GET /api/countries/GetCountryByCurrencyCode?code={code}` - Retrieves country information by currency code.
+- `GET /api/countries/GetCountryByPhoneCode?code={code}` - Retrieves country information by phone code.
+- `GET /api/countries/GetCountryEmojiFlag?code={code}` - Retrieves the emoji flag for a country by country code.
+- `GET /api/countries/GetCurrencyCodesByCountryCode?code={code}` - Retrieves currency codes by country code.
+- `GET /api/countries/GetPhoneCodeByCountryShortCode?code={code}` - Retrieves phone code by country short code.
+- `GET /api/countries/GetRegionByCountryCode?code={code}` - Retrieves region information by country code.
+
+## Contributing
+
+Contributions are welcome! Please open an issue or submit a pull request for any changes.
+
+## License
+
+This project is licensed under the MIT License.
